19fa29f7b39dc56f66f986af2e09647b1b9dcc1a
lost-inside <49042740+lost-inside@users.noreply.github.com>
2019-09-15 23:15:28 +0530

    Update fi.py

--- fi.py
+++ fi.py
@@ -1,8 +1,9 @@
-x=input('say your name ').
-/ /
-print('Hello {}'.format(x))
-print('this') as well as and this vvs well  
-//
-
- # this is the world we had in mind
-
+asdfa
+asd
+fas
+dfa
+sdf
+asd
+fa
+sdfas
+dfasdf
